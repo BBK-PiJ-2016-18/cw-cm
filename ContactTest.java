bcb29e78da0b24b75fc2f66a9bb4e0a9b1bb545a
phewso01 <phewso01@dcs.bbk.ac.uk>
2017-02-08 18:17:34 +0000

    updates

--- ContactTest.java
+++ ContactTest.java
@@ -2,7 +2,13 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class ContactTest {
+	private Contact contact;
+	
 	// setup
+	@Before
+	public void setUp() {
+		Contact contact = new ContactImpl(33, "John Smith", "some notes");
+	}
 	
 	//test
 	@Test
@@ -15,5 +21,15 @@ public class ContactTest {
 		ContactImpl testContact2 = new ContactImpl(18, "Jane Bloggs");
 		assertEquals(testContact2.getId(), 18);
 		assertEquals(testContact2.getName(), "Jane Bloggs");
+	}
+	
+	@Test
+	public void testsGetter() {
+		int id = contact.getId();
+		assertEquals(id, 33);
+		String name = contact.getName();
+		String notes = contact.getNotes();
+		assertEquals(name, "John Smith");
+		assertEquals(notes, "some notes");
 	}
 }
